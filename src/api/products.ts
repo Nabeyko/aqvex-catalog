84be6da47a9689b7803e99fbcc610802a44cc50f
Nazar Beyko <nazarbeyko12@gmail.com>
2026-03-13 13:27:07 +0200

    use local products.json instead of API due to CORS issue

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,7 +1,6 @@
 import type { Product, ProductsResponse } from '../types/product';
 
-const BASE_URL =
-  'https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1/products';
+const BASE_URL = '/products.json';
 
 export const getProducts = async (): Promise<Product[]> => {
   const response = await fetch(BASE_URL);
